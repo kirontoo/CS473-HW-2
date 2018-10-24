a7c9a0f8f892541e25b065d0ae6dfa9fb832590c
Amy Dang <kirontoo@gmail.com>
2018-10-24 01:47:32 -0700

    add function to parse JSON for student based on first and last name

--- StudentModel.js
+++ StudentModel.js
@@ -2,7 +2,7 @@ var StudentModel = function StudentModel(XMLHttpRequest) {
     this.XMLHttpRequest = XMLHttpRequest;
 }
 
-// Get student based on last name
+// Parse JSON data to search for students based on last name
 StudentModel.prototype.getStudents = function getStudents(lname, fn) {
     var oReq = new this.XMLHttpRequest();
     oReq.overrideMimeType("application/json");
@@ -24,6 +24,34 @@ StudentModel.prototype.getStudents = function getStudents(lname, fn) {
         });
 
         fn(students);
+    };
+
+    oReq.open('GET', 'example-data.json', true);
+    oReq.send();
+}
+
+// Parse JSON data to search for students based on first and last name
+StudentModel.prototype.getGrades = function getGrades(fname, lname, fn) {
+    var oReq = new this.XMLHttpRequest();
+    oReq.overrideMimeType("application/json");
+
+    // Load JSON data
+    oReq.onload = function onLoad(e) {
+
+        // parse JSON data
+        var response = JSON.parse(e.target.responseText);
+
+        var list = response.students;
+        var student = {};
+
+        // Find all students with the same first and last name
+        list.forEach( (s) => {
+            if (s.lname === lname && s.fname == fname) {
+                student = s;
+            }
+        });
+
+        fn(student);
     };
 
     oReq.open('GET', 'example-data.json', true);
