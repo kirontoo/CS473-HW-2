0e045e1b2bbf12d9fdc7bb21dc1800ff6affbeb3
Amy Dang <kirontoo@gmail.com>
2018-10-24 00:59:56 -0700

    add event listener to search for students

--- StudentController.js
+++ StudentController.js
@@ -7,7 +7,31 @@ StudentController.prototype.init = function init() {
     this.studentView.onClickGetStudents = this.onClickGetStudents.bind(this);
 };
 
+// Search for students on submission
 StudentController.prototype.onClickGetStudents = function onClickGetStudents(event) {
 
+    // Grab the input value
+    target = document.getElementById("lname").value
+    this.studentModel.getStudents(target, this.showFoundStudents.bind(this));
+};
+
+// Show all found students
+StudentController.prototype.showFoundStudents = function showFoundStudents(studentModelData) {
+
+    // Create data to render
+    var studentViewModelList = [];
+
+    studentModelData.forEach( (s) => {
+        var studentViewModel = {
+            fname: s.fname,
+            lname: s.lname,
+            course: s.course,
+            gpa: s.gpa
+        };
+
+        studentViewModelList.push(studentViewModel);
+    });
     
-}
+    // Render the model
+    this.studentView.renderStudents(studentViewModelList);
+};
--- StudentModel.js
+++ StudentModel.js
@@ -7,20 +7,28 @@ StudentModel.prototype.getStudents = function getStudents(lname, fn) {
     var oReq = new this.XMLHttpRequest();
     oReq.overrideMimeType("application/json");
 
+    // Load JSON data
     oReq.onload = function onLoad(e) {
-        var response = JSON.parse(e.target.responseText);
-        var students = [];
 
+        // parse JSON data
+        var response = JSON.parse(e.target.responseText);
+
+        var students = [];
         var list = response.students;
 
+        // Find all students with the same last name
         list.forEach( (student) => {
             if (student.lname === lname) {
                 students.push(student)
             }
         });
+
+        console.log(students);
+
+        // Call showStudents
+        fn(students);
     };
 
     oReq.open('GET', 'example-data.json', true);
     oReq.send();
 }
-
